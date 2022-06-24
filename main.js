84735da63e9ba2c126d908ace2a54ee904757b45
Dzakiy Dzakwan <dzakcart@gmail.com>
2022-06-24 11:08:26 +0700

    Commit-6

--- main.js
+++ main.js
@@ -41,6 +41,78 @@ let mangaList = [
   },
 ];
 
+let title = "";
+let currentPage = 1;
+let newPage = 0;
+let activePages = 1;
+const cardContainer = document.querySelector("#card-container");
+const paginationContainer = document.querySelector("#pagination-container");
+
+/* console.log(statusContainer); */
+
+//Filter
+let orderList = [
+  "favorites",
+  "rank",
+  "popularity",
+  "volumes",
+  "title",
+  "scores",
+];
+
+let orderBy = "favorites";
+const orderByContainer = document.querySelector("#order-by");
+orderList.forEach((item) => {
+  /* console.log(item); */
+  orderByContainer.innerHTML += `<option value="${item}">${
+    item[0].toUpperCase() + item.substring(1, item.length)
+  }</option>`;
+});
+
+let typeList = [
+  "manga",
+  "novel",
+  "lightnovel",
+  "oneshot",
+  "doujin",
+  "manhwa",
+  "manhua",
+];
+
+let type = "";
+const typeContainer = document.querySelector("#type");
+typeList.forEach((item) => {
+  /* console.log(item); */
+  typeContainer.innerHTML += `<option value="${item}">${
+    item[0].toUpperCase() + item.substring(1, item.length)
+  }</option>`;
+});
+
+let statusList = [
+  "publishing",
+  "complete",
+  "hiatus",
+  "discontinued",
+  "upcoming",
+];
+
+let status = "";
+const statusContainer = document.querySelector("#status");
+statusList.forEach((item) => {
+  /* console.log(item); */
+  statusContainer.innerHTML += `<option value="${item}">${
+    item[0].toUpperCase() + item.substring(1, item.length)
+  }</option>`;
+});
+
+//genres
+/* fetch("https://api.jikan.moe/v4/genres/manga").then((response) => {
+  return response.json().then((data) => {
+    console.log(data.data);
+  });
+}); */
+
+//Limit
 let limit = 0;
 
 if (screen.width > 600 && screen.width <= 900) {
@@ -51,17 +123,101 @@ if (screen.width > 600 && screen.width <= 900) {
   limit = 10;
 }
 
-$.ajax({
+function getData(limit, orderBy, title, currentPage) {
+  fetch(
+    `https://api.jikan.moe/v4/manga?sort=desc&limit=${limit}&order_by=${orderBy}&q=${title}&page=${currentPage}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      /* console.log(data.pagination.current_page); */
+      activePages = data.pagination.current_page;
+      cardContainer.innerHTML = ``;
+      data.data.forEach((item, index) => {
+        cardContainer.innerHTML += `
+        <div
+        class="card flex flex-col justify-between"
+        style = "background-image:url('${item.images.jpg.image_url}')"
+      >
+        <div
+          class="bg-secondary-80 inline-block ml-auto w-[25px] h-[25px] text-center text-18 text-teritary rounded-md"
+        >
+          ${index + 1}
+        </div>
+        <div
+          class="bg-teritary-80 text-14 text-secondary text-center rounded-lg"
+        >
+          ${item.title}
+        </div>
+      </div>
+        `;
+        /* console.log(item); */
+      });
+    })
+    .catch((Err) => {
+      console.log(Err);
+    });
+}
+
+/* Pagination */
+let paginationNumberList = 20;
+
+screen.width <= 900 ? (paginationNumberList = 10) : (paginationNumberList = 20);
+
+for (let i = 1; i <= paginationNumberList; i++) {
+  let activeNumber = ``;
+
+  if (i == currentPage) {
+    activeNumber = ` <li class="pagination-list e">
+    <button
+      class="rounded-full bg-teritary w-[30px] h-[30px] flex items-center justify-center text-base cursor-pointer hover:shadow-2xl active"
+    >
+      ${i}
+    </button>
+  </li>`;
+  } else {
+    activeNumber = ` <li class="pagination-list e">
+    <button
+      class="rounded-full bg-teritary w-[30px] h-[30px] flex items-center justify-center text-base cursor-pointer hover:shadow-2xl"
+    >
+      ${i}
+    </button>
+  </li>`;
+  }
+
+  paginationContainer.innerHTML += activeNumber;
+}
+
+let paginationList = document.querySelectorAll(".pagination-list");
+
+paginationList.forEach((index, parameter) => {
+  index.addEventListener("click", () => {
+    console.log(`success ${parameter + 1}`);
+  });
+});
+
+getData(limit, orderBy, title, currentPage);
+
+/* console.log(paginationList); */
+
+//Jquery Ajax
+
+/* $.ajax({
   url: "https://api.jikan.moe/v4/manga",
   type: "GET",
   dataType: "JSON",
   data: {
     limit: limit,
-    order_by: "favorites",
+    order_by: orderBy,
     sort: "desc",
   },
 
   success: (data) => {
+    console.log(data);
     $.each(data.data, (index, item) => {
       console.log(item);
       $("#card-container").append(`
@@ -83,8 +239,9 @@ $.ajax({
       `);
     });
   },
-});
+}); */
 
+//Data dummy
 /* $.each(mangaList, (index, item) => {
   $("#card-container").append(`
   <div
@@ -104,5 +261,3 @@ $.ajax({
 </div>
   `);
 }); */
-
-console.log(screen.width);
